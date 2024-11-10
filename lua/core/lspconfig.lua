d366f53315e8860623fb85997e2720c52313d4a4
Vitaly (wsl) <obergodmar@gmail.com>
2024-11-10 05:22:37 +0300

    feat: add devops nvim installation

--- lua/core/lspconfig.lua
+++ lua/core/lspconfig.lua
@@ -1,36 +1,8 @@
 local on_attach = require('helpers.lsp.common').on_attach
 
 local servers = {
-  marksman = {},
-  html = {},
-  cssls = {},
   typos_lsp = {},
-  cssmodules_ls = {},
-  stylelint_lsp = {
-    stylelintplus = {
-      autoFixOnSave = false,
-      autoFixOnFormat = true,
-    },
-  },
-  eslint = {},
-  lua_ls = {
-    Lua = {
-      workspace = { checkThirdParty = false },
-      telemetry = { enable = false },
-      hint = { enable = true },
-    },
-  },
   bashls = {},
-  gopls = {
-    gopls = {
-      ['ui.inlayhint.hints'] = {
-        compositeLiteralFields = true,
-        constantValues = true,
-        parameterNames = true,
-      },
-    },
-  },
-  intelephense = {},
 }
 
 ---@type LazyPluginSpec
@@ -43,61 +15,7 @@ local P = {
     'williamboman/mason.nvim',
     -- Extension to mason.nvim that makes it easier to use lspconfig with mason.nvim.
     'williamboman/mason-lspconfig.nvim',
-    {
-      'folke/lazydev.nvim',
-      ft = 'lua', -- only load on lua files
-      opts = {
-        library = {
-          'lazy.nvim',
-        },
       },
-    },
-    {
-      'pmizio/typescript-tools.nvim',
-      dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
-      opts = {
-        on_attach = require('helpers.lsp.common').on_attach,
-        settings = {
-          tsserver_logs = 'verbose',
-          separate_diagnostic_server = false,
-          publish_diagnostic_on = 'insert_leave',
-          expose_as_code_action = {
-            'fix_all',
-            'add_missing_imports',
-            'remove_unused',
-          },
-          tsserver_path = nil,
-          tsserver_plugins = {},
-          tsserver_max_memory = 4096,
-          tsserver_format_options = {},
-          complete_function_calls = true,
-          jsx_close_tag = {
-            enable = true,
-            filetypes = { 'javascriptreact', 'typescriptreact' },
-          },
-          tsserver_file_preferences = function()
-            return {
-              includeCompletionsForImportStatements = true,
-              includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-              includeInlayFunctionParameterTypeHints = true,
-              includeInlayVariableTypeHints = true,
-              includeInlayPropertyDeclarationTypeHints = true,
-              includeInlayFunctionLikeReturnTypeHints = true,
-              includeInlayEnumMemberValueHints = true,
-              importModuleSpecifierPreference = 'auto',
-              includeInlayParameterNameHints = 'all',
-              includeCompletionsForModuleExports = true,
-              quotePreference = 'auto',
-            }
-          end,
-        },
-      },
-    },
-    {
-      'mfussenegger/nvim-jdtls',
-      ft = 'java',
-    },
-  },
   config = function()
     local capabilities = require('helpers.lsp.common').get_capabilities()
 
@@ -116,78 +34,10 @@ local P = {
     mason_lspconfig.setup_handlers({
       function(server_name)
         local lspconfig = require('lspconfig')
-
-        local root_dir = nil
-        local init_options = nil
-        local filetypes = nil
-        local commands = nil
-        local handlers = nil
-        local autostart = true
-
-        if server_name == 'lua_ls' then
-          root_dir = lspconfig.util.root_pattern('.git', '*.rockspec')
-        end
-
-        if server_name == 'gopls' then
-          root_dir = lspconfig.util.root_pattern('go.work', 'go.mod', '.git')
-        end
-
-        if server_name == 'stylelint_lsp' then
-          filetypes = {
-            'css',
-            'less',
-            'scss',
-            'sugarss',
-            'wxss',
-            -- "javascript",
-            -- "javascriptreact",
-            -- "typescript",
-            -- "typescriptreact",
-          }
-
-          root_dir = lspconfig.util.root_pattern('.stylelintrc', 'package.json', '.git')
-        end
-
-        if server_name == 'eslint' then
-          filetypes = {
-            'javascript',
-            'javascriptreact',
-            'typescript',
-            'typescriptreact',
-          }
-        end
-
-        if server_name == 'intelephense' or server_name == 'phpactor' then
-          autostart = false
-        end
-
-        if server_name == 'java_language_server' then
-          handlers = {
-            ['client/registerCapability'] = function(err, result, ctx, config)
-              local registration = {
-                registrations = { result },
-              }
-              return vim.lsp.handlers['client/registerCapability'](err, registration, ctx, config)
-            end,
-          }
-        end
-
-        if server_name == 'typos_lsp' then
-          init_options = {
-            diagnosticSeverity = 'Hint',
-          }
-        end
-
         lspconfig[server_name].setup({
           settings = servers[server_name],
           capabilities = capabilities,
           on_attach = on_attach,
-          root_dir = root_dir,
-          init_options = init_options,
-          filetypes = filetypes,
-          commands = commands,
-          autostart = autostart,
-          handlers = handlers,
         })
 
         vim.diagnostic.config({
--- lua/plugins/tailwind-tools.lua
+++
@@ -1,9 +0,0 @@
----@type LazyPluginSpec
-local P = {
-  'luckasRanarison/tailwind-tools.nvim',
-  name = 'tailwind-tools',
-  build = ':UpdateRemotePlugins',
-  opts = {}, -- your configuration
-}
-
-return P
