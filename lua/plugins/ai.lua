b17266edeb9f3cca62323fcd318bd942218a282f
obergodmar <obergodmar@gmail.com>
2026-04-20 13:18:54 +0300

    feat(ai): manually enable copilot

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,8 +1,70 @@
+local copilot_filetypes = {
+  lua = true,
+  nix = true,
+  sh = true,
+  bash = true,
+  zsh = true,
+  json = true,
+  jsonc = true,
+  xml = true,
+  css = true,
+  html = true,
+  javascript = true,
+  javascriptreact = true,
+  typescript = true,
+  typescriptreact = true,
+  svelte = true,
+  vue = true,
+  php = true,
+  go = true,
+  rust = true,
+  c = true,
+  cpp = true,
+  ['*'] = false,
+}
+
+local function is_copilot_filetype_enabled()
+  local filetype = vim.bo.filetype
+  local short_filetype = filetype:gsub('%..*', '')
+
+  return copilot_filetypes[filetype] == true or copilot_filetypes[short_filetype] == true
+end
+
+local function toggle_copilot()
+  if package.loaded['copilot'] and not require('copilot.client').is_disabled() then
+    require('copilot.command').disable()
+    vim.notify('Copilot disabled', vim.log.levels.INFO)
+    return
+  end
+
+  if not is_copilot_filetype_enabled() then
+    vim.notify('Copilot not enabled for filetype: ' .. vim.bo.filetype, vim.log.levels.WARN)
+    return
+  end
+
+  require('lazy').load({ plugins = { 'copilot.lua' } })
+
+  local command = require('copilot.command')
+
+  if require('copilot.client').is_disabled() then
+    local should_attach, no_attach_reason = require('copilot.util').should_attach(0)
+
+    if not should_attach then
+      vim.notify('Copilot not enabled: ' .. no_attach_reason, vim.log.levels.WARN)
+      return
+    end
+
+    command.enable()
+  end
+
+  command.attach()
+  vim.notify('Copilot enabled', vim.log.levels.INFO)
+end
+
 return {
   {
     'zbirenbaum/copilot.lua',
-    cmd = 'Copilot',
-    event = 'InsertEnter',
+    lazy = true,
     dependencies = {
       {
         'copilotlsp-nvim/copilot-lsp',
@@ -11,7 +73,12 @@ return {
         end,
       },
     },
+    init = function()
+      vim.keymap.set('n', '<leader>C', toggle_copilot, { desc = 'Toggle Copilot' })
+      vim.keymap.set('n', '<leader>С', toggle_copilot, { desc = 'Toggle Copilot' })
+    end,
     opts = {
+      filetypes = copilot_filetypes,
       panel = {
         enabled = false,
       },
@@ -43,109 +110,10 @@ return {
       },
     },
     config = function(_, opts)
-      if require('helpers.utils').is_mac() then
-        opts.copilot_node_command = vim.fn.expand('$HOME') .. '/.nvm/versions/node/v22.21.0/bin/node'
-      end
-      require('copilot').setup(opts)
-
       vim.g.copilot_no_tab_map = true
-      vim.g.copilot_proxy = '127.0.0.1:12334'
-    end,
-  },
-  {
-    'obergodmar/gp.nvim',
-    enabled = false,
-    config = function()
-      local conf = {
-        openai_api_key = os.getenv('OAI_API_KEY'),
-        providers = {
-          openai = {
-            disable = false,
-            endpoint = 'https://api.openai.com/v1/chat/completions',
-          },
-        },
-        -- curl_params = { "--proxy", "http://127.0.0.1:12334" }
-      }
+      -- vim.g.copilot_proxy = '127.0.0.1:12334'
 
-      require('gp').setup(conf)
-      require('which-key').add({
-        -- VISUAL mode mappings
-        -- s, x, v modes are handled the same way by which_key
-        {
-          mode = { 'v' },
-          nowait = true,
-          remap = false,
-          { '<C-g><C-t>', ":<C-u>'<,'>GpChatNew tabnew<cr>", desc = 'ChatNew tabnew' },
-          { '<C-g><C-v>', ":<C-u>'<,'>GpChatNew vsplit<cr>", desc = 'ChatNew vsplit' },
-          { '<C-g><C-x>', ":<C-u>'<,'>GpChatNew split<cr>", desc = 'ChatNew split' },
-          { '<C-g>a', ":<C-u>'<,'>GpAppend<cr>", desc = 'Visual Append (after)' },
-          { '<C-g>b', ":<C-u>'<,'>GpPrepend<cr>", desc = 'Visual Prepend (before)' },
-          { '<C-g>c', ":<C-u>'<,'>GpChatNew<cr>", desc = 'Visual Chat New' },
-          { '<C-g>g', group = 'generate into new ..' },
-          { '<C-g>ge', ":<C-u>'<,'>GpEnew<cr>", desc = 'Visual GpEnew' },
-          { '<C-g>gn', ":<C-u>'<,'>GpNew<cr>", desc = 'Visual GpNew' },
-          { '<C-g>gp', ":<C-u>'<,'>GpPopup<cr>", desc = 'Visual Popup' },
-          { '<C-g>gt', ":<C-u>'<,'>GpTabnew<cr>", desc = 'Visual GpTabnew' },
-          { '<C-g>gv', ":<C-u>'<,'>GpVnew<cr>", desc = 'Visual GpVnew' },
-          { '<C-g>i', ":<C-u>'<,'>GpImplement<cr>", desc = 'Implement selection' },
-          { '<C-g>n', '<cmd>GpNextAgent<cr>', desc = 'Next Agent' },
-          { '<C-g>p', ":<C-u>'<,'>GpChatPaste<cr>", desc = 'Visual Chat Paste' },
-          { '<C-g>r', ":<C-u>'<,'>GpRewrite<cr>", desc = 'Visual Rewrite' },
-          { '<C-g>s', '<cmd>GpStop<cr>', desc = 'GpStop' },
-          { '<C-g>t', ":<C-u>'<,'>GpChatToggle<cr>", desc = 'Visual Toggle Chat' },
-          { '<C-g>x', ":<C-u>'<,'>GpContext<cr>", desc = 'Visual GpContext' },
-        },
-
-        -- NORMAL mode mappings
-        {
-          mode = { 'n' },
-          nowait = true,
-          remap = false,
-          { '<C-g><C-t>', '<cmd>GpChatNew tabnew<cr>', desc = 'New Chat tabnew' },
-          { '<C-g><C-v>', '<cmd>GpChatNew vsplit<cr>', desc = 'New Chat vsplit' },
-          { '<C-g><C-x>', '<cmd>GpChatNew split<cr>', desc = 'New Chat split' },
-          { '<C-g>a', '<cmd>GpAppend<cr>', desc = 'Append (after)' },
-          { '<C-g>b', '<cmd>GpPrepend<cr>', desc = 'Prepend (before)' },
-          { '<C-g>c', '<cmd>GpChatNew<cr>', desc = 'New Chat' },
-          { '<C-g>f', '<cmd>GpChatFinder<cr>', desc = 'Chat Finder' },
-          { '<C-g>g', group = 'generate into new ..' },
-          { '<C-g>ge', '<cmd>GpEnew<cr>', desc = 'GpEnew' },
-          { '<C-g>gn', '<cmd>GpNew<cr>', desc = 'GpNew' },
-          { '<C-g>gp', '<cmd>GpPopup<cr>', desc = 'Popup' },
-          { '<C-g>gt', '<cmd>GpTabnew<cr>', desc = 'GpTabnew' },
-          { '<C-g>gv', '<cmd>GpVnew<cr>', desc = 'GpVnew' },
-          { '<C-g>n', '<cmd>GpNextAgent<cr>', desc = 'Next Agent' },
-          { '<C-g>r', '<cmd>GpRewrite<cr>', desc = 'Inline Rewrite' },
-          { '<C-g>s', '<cmd>GpStop<cr>', desc = 'GpStop' },
-          { '<C-g>t', '<cmd>GpChatToggle<cr>', desc = 'Toggle Chat' },
-          { '<C-g>x', '<cmd>GpContext<cr>', desc = 'Toggle GpContext' },
-        },
-
-        -- INSERT mode mappings
-        -- {
-        --   mode = { 'i' },
-        --   nowait = true,
-        --   remap = false,
-        --   { '<C-g><C-t>', '<cmd>GpChatNew tabnew<cr>', desc = 'New Chat tabnew' },
-        --   { '<C-g><C-v>', '<cmd>GpChatNew vsplit<cr>', desc = 'New Chat vsplit' },
-        --   { '<C-g><C-x>', '<cmd>GpChatNew split<cr>', desc = 'New Chat split' },
-        --   { '<C-g>a', '<cmd>GpAppend<cr>', desc = 'Append (after)' },
-        --   { '<C-g>b', '<cmd>GpPrepend<cr>', desc = 'Prepend (before)' },
-        --   { '<C-g>c', '<cmd>GpChatNew<cr>', desc = 'New Chat' },
-        --   { '<C-g>f', '<cmd>GpChatFinder<cr>', desc = 'Chat Finder' },
-        --   { '<C-g>g', group = 'generate into new ..' },
-        --   { '<C-g>ge', '<cmd>GpEnew<cr>', desc = 'GpEnew' },
-        --   { '<C-g>gn', '<cmd>GpNew<cr>', desc = 'GpNew' },
-        --   { '<C-g>gp', '<cmd>GpPopup<cr>', desc = 'Popup' },
-        --   { '<C-g>gt', '<cmd>GpTabnew<cr>', desc = 'GpTabnew' },
-        --   { '<C-g>gv', '<cmd>GpVnew<cr>', desc = 'GpVnew' },
-        --   { '<C-g>n', '<cmd>GpNextAgent<cr>', desc = 'Next Agent' },
-        --   { '<C-g>r', '<cmd>GpRewrite<cr>', desc = 'Inline Rewrite' },
-        --   { '<C-g>s', '<cmd>GpStop<cr>', desc = 'GpStop' },
-        --   { '<C-g>t', '<cmd>GpChatToggle<cr>', desc = 'Toggle Chat' },
-        --   { '<C-g>x', '<cmd>GpContext<cr>', desc = 'Toggle GpContext' },
-        -- },
-      })
+      require('copilot').setup(opts)
     end,
   },
 }
