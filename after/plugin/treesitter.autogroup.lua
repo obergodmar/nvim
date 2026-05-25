77990cd4a5a967574726c38787896a51dc79471d
Vitaly <obergodmar@gmail.com>
2026-05-25 18:11:03 +0300

    feat(treesitter): update plugin and extend default auto-install list

--- after/plugin/treesitter.autogroup.lua
+++
@@ -1,8 +0,0 @@
-local ts = vim.api.nvim_create_augroup('ts-highlight', { clear = true })
-
-vim.api.nvim_create_autocmd('FileType', {
-  group = ts,
-  callback = function(args)
-    pcall(vim.treesitter.start, args.buf)
-  end,
-})
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -21,17 +21,150 @@ return {
     },
 
     config = function()
-      require('nvim-treesitter').setup({})
-      require('nvim-treesitter').install({
+      local treesitter = require('nvim-treesitter')
+      local parsers = {
+        'bash',
+        'c',
+        'c_sharp',
+        'clojure',
+        'cmake',
+        'comment',
+        'cpp',
+        'css',
+        'csv',
+        'dart',
+        'desktop',
+        'diff',
+        'dockerfile',
+        'ecma',
+        'editorconfig',
+        'elixir',
+        'erlang',
+        'git_config',
+        'gitattributes',
         'gitcommit',
+        'gitignore',
+        'go',
+        'graphql',
+        'groovy',
+        'haskell',
+        'hcl',
+        'html',
+        'http',
+        'ini',
+        'java',
+        'javascript',
+        'jq',
+        'json',
+        'json5',
+        'jsx',
+        'just',
+        'kdl',
+        'kotlin',
+        'lua',
+        'luadoc',
+        'make',
         'markdown',
         'markdown_inline',
+        'nginx',
+        'nix',
+        'ocaml',
+        'passwd',
+        'php',
+        'prisma',
+        'properties',
+        'python',
+        'query',
         'regex',
-        'lua',
+        'ruby',
+        'rust',
+        'scss',
+        'sql',
+        'ssh_config',
+        'svelte',
+        'swift',
+        'terraform',
+        'toml',
+        'tsx',
+        'typescript',
+        'udev',
         'vim',
-        'bash',
-        'comment',
+        'vimdoc',
+        'vue',
+        'xml',
+        'yaml',
+        'zig',
+      }
+
+      treesitter.setup({})
+      local install_task = treesitter.install(parsers)
+
+      vim.filetype.add({
+        extension = {
+          cfg = 'cfg',
+          conf = 'conf',
+        },
       })
+      vim.treesitter.language.register('ini', { 'cfg', 'conf' })
+
+      local available_parsers = {}
+      for _, parser in ipairs(treesitter.get_available()) do
+        available_parsers[parser] = true
+      end
+
+      local installing = {}
+
+      local function restart_treesitter(buf, lang)
+        if not vim.api.nvim_buf_is_valid(buf) then
+          return
+        end
+
+        pcall(vim.treesitter.stop, buf)
+        pcall(vim.treesitter.start, buf, lang)
+      end
+
+      local function start_or_install(args)
+        local lang = vim.treesitter.language.get_lang(args.match)
+        if not lang or not available_parsers[lang] then
+          return
+        end
+
+        if pcall(vim.treesitter.start, args.buf, lang) or installing[lang] then
+          return
+        end
+
+        installing[lang] = true
+        treesitter.install({ lang }):await(function(err, ok)
+          installing[lang] = nil
+          if err or not ok then
+            return
+          end
+
+          vim.schedule(function()
+            restart_treesitter(args.buf, lang)
+          end)
+        end)
+      end
+
+      vim.api.nvim_create_autocmd('FileType', {
+        group = vim.api.nvim_create_augroup('ts-auto-install', { clear = true }),
+        callback = start_or_install,
+      })
+
+      install_task:await(function(err, ok)
+        if err or not ok then
+          return
+        end
+
+        vim.schedule(function()
+          for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+            local lang = vim.treesitter.language.get_lang(vim.bo[buf].filetype)
+            if available_parsers[lang] then
+              restart_treesitter(buf, lang)
+            end
+          end
+        end)
+      end)
 
       local ts_move = require('nvim-treesitter-textobjects.move')
       vim.keymap.set({ 'n', 'x', 'o' }, ']f', function()
